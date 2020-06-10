1328269e79ae06f9fe0c382570f89831f8769fdb
Pathum312 <pathum.2019557@gmail.com>
2020-06-10 12:09:24 +0530

    Sixth Change

--- Task1.java
+++ Task1.java
@@ -4,19 +4,9 @@ import java.util.Random;
 
 public class Task1 {
     public static void main(String[] args) {
-       Random random = new Random();
-       int count = 1;
-       int maxLimit = 11;
-       int num = 1;
-
-       do {
-           num = random.nextInt(maxLimit);
-           if (num == 7) {
-               System.out.println("Lucky 7!");
-           }else {
-               System.out.println("Number "+count+" was "+num);
-           }
-           count += 1;
-       }while (num != 0);
+       System.out.println("Hello World!")
+	   System.out.println("Welcome to GitHub.")
+	   System.out.println("pathum.2019557@gmail.com")
+	   System.out.println("Added Task1.java")
     }
 }
